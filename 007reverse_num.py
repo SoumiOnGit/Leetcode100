9029839eec8ef28aa5364bd3650852d6a52694e9
Soumi Sarkar <90122541+SoumiOnGit@users.noreply.github.com>
2024-11-26 21:21:17 +0530

    Update 007reverse_num.py

--- 007reverse_num.py
+++ 007reverse_num.py
@@ -1,17 +1,18 @@
 '''
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
 Example 1:
 
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
+Input: x = 123
+Output: 321
 Example 2:
 
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Input: x = -123
+Output: -321
 Example 3:
 
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+Input: x = 120
+Output: 21
 '''
